15723cced5b5c80147cf4328a1d01ee67f7d311c
lportillo896 <lportillo896@mycr.redwoods.edu>
2023-02-03 19:21:36 -0800

    Completed Java Lab 003

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -10,5 +10,11 @@ public class GettingHotInHere {
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is the temperature in °F: ");
         int number = scanner.nextInt();
+        float celsius = (number - 32.0f) * (5.0f/9.0f);
+        int con = (int)celsius;
+        System.out.println(number + "`F");
+        System.out.println(celsius + "`C");
+        System.out.println(con + "`C");
+        System.out.println("If it were 2`C warmer it would be: " + (2 + celsius) + "`C");
     }
 }
--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -15,5 +15,8 @@ public class MrRoboto {
 
         System.out.print("My name is Mr. Roboto, are you Kilroy? ");
         // Write your program here
+        String no = scanner.nextLine();
+        System.out.println(domo + no);
+
     }
 }
